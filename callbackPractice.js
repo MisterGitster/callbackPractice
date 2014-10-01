e07ec190f7aaf7681ca9c2357a89dc58a8ee3999
Josh Butler <JButler@jbutler-mac.ctac.1800contacts.com>
2014-10-01 12:48:30 -0600

    cb functions practice

--- callbackPractice.js
+++ callbackPractice.js
@@ -22,6 +22,9 @@ and what you should write is the favNum function that makes the code above work,
     
 */
 
+var first = function(myArray, cb) {
+    cb(myArray[0]);
+}
 
 
   //Code Here for first
@@ -36,6 +39,9 @@ first(names, function(firstName){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
+var last = function(myArray, cb) {
+    cb(myArray[myArray.length - 1]);
+}
 
 
   //Code Here for last
@@ -52,7 +58,9 @@ last(names, function(lastName){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
+var multiply = function(firstNum, secondNum, cb) {
+  cb(firstNum * secondNum);
+}
 
 
 
@@ -69,17 +77,24 @@ multiply(4, 3, function(answer){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
+var contains = function(myArray, str, cb) {
+  for (var i = 0; i < myArray.length; i++) {
+    if (myArray[i] === str) {
+      return cb(true);
+    }
+  };
+  cb(false)
+};
 
 
   //Code Here for contains
 
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(yes){
   if(yes === true){
-    'Colt is in the array';
+    console.log('Colt is in the array');
   } else {
-    'Colt is not in the array';
+    console.log('Colt is not in the array');
   }
 });
 
@@ -90,8 +105,39 @@ contains(names, 'Colt', function(yes){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
+var uniq = function(myArray, cb) {
+  var duplicateArray = [];
+  for (var i = 0; i < myArray.length; i++) {
+    for (var j = (i + 1); j < myArray.length; j++) {
+      if (myArray[i] === myArray[j]) {
+        var match = myArray.splice(j,1);
+        duplicateArray.push(match);
+      }
+    }
+  }
+  return cb(myArray);
+};
 
 
+// var uniq = function(arr, callback) {
+//   var newArr =[];
+//   var flag;
+//   for(var i = 0; i < arr.length; i++) {
+//       for(var j = 0; j < newArr.length; j++) {
+//          if (arr[i] == newArr[j]) {
+//               flag = true;
+//               break;
+//          }
+//       }
+//       if (flag == false) {
+//         newArr.push(arr[i]);
+//       }
+//   else {
+//     flag = false;
+//   }
+// } 
+// callback(newArr);
+// };
     //Code Here for uniq
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -101,18 +147,20 @@ uniq(names, function(uniqArr){
 
 
 
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
+var each = function(myArray, cb) {
+  for (var i = 0; i < myArray.length; i++) {
+    cb(myArray[i], i);
+  }
+}
 
 
     //Code Here for each
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
@@ -122,7 +170,13 @@ each(names, function(item, indice){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
+var getUserById = function(str, cb) {
+  for (var i = 0; i < users.length; i++) {
+    if (str === users[i].id) {
+      cb(users[i]);
+    }
+  }
+}
 
 
  //code here for getUserById
